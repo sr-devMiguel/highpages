defbe025315a4d638a3f2375f6d0f15567308aab
sr-devMiguel <devmiguel23@gmail.com>
2025-12-12 12:32:08 -0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: 'frontend',
   plugins: [react()],
-  base: "/highpages/"
+  server: { port: 5173 },
 })
